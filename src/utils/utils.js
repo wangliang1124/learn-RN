f03f8126a192db2f569f964cbe372098724dde04
王亮 <wangliang1124@163.com>
2021-03-31 15:17:01 +0800

    move utils.js to util directory

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,37 +1,33 @@
 // obtained from react native tutorials
-
-import React from 'react';
-import { PixelRatio } from 'react-native';
-import Dimensions from 'Dimensions';
+import { Dimensions, PixelRatio } from 'react-native'
 
 const Util = {
   ratio: PixelRatio.get(),
   pixel: 1 / PixelRatio.get(),
   size: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
+    height: Dimensions.get('window').height,
   },
   post(url, data, callback) {
     const fetchOptions = {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
-    };
+      body: JSON.stringify(data),
+    }
 
     fetch(url, fetchOptions)
-    .then((response) => {
-      return response.json() 
-    })
-    .then((responseData) => {
-      callback(responseData);
-    });
+      .then((response) => {
+        return response.json()
+      })
+      .then((responseData) => {
+        callback(responseData)
+      })
   },
   key: 'BDKHFSDKJFHSDKFHWEFH-REACT-NATIVE',
-};
-
+}
 
 // import {StyleSheet, Platform} from 'react-native';
 
@@ -50,4 +46,4 @@ const Util = {
 //   return StyleSheet.create(platformStyles);
 // }
 
-export default Util;
+export default Util
